7c82641f2077f88ec59b9be0258b245f268640da
Saur8055 <sawantsaurabh164@gmail.com>
2022-12-17 21:01:28 +0530

    favicon changed

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
     return(
         <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
             <div className="container-fluid">
-                <a className="navbar-brand ms-3" href="/" style={{color: props.mode==='dark'?'white':'black'}}><strong>{props.title}</strong><small> (by s@urhubbb)</small></a>
+                <a className="navbar-brand ms-3" href="/" style={{color: props.mode==='dark'?'white':'black'}}><strong>{props.title}</strong></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
